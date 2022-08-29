d74e535ea8122af6feda0cab3cb149b31a71f24e
Nicolas BEURIOT <nbeuriot@gmail.com>
2022-08-29 17:48:54 +0200

    Add new Van

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,22 @@ Rails.application.routes.draw do
   # root "articles#index"
   # get "restaurants/:id", to: "restaurants#show", as: :restaurant
   get "vans", to: "vans#index", as: :vans
+  get "vans/new", to: "vans#new", as: :new_vans
+  post "vans", to: "vans#create"
 end
+
+
+
+# # CRUD
+#   # READ ALL
+#   get 'tasks', to: 'tasks#index', as: :index
+#   # CREATE
+#   get 'tasks/new', to: 'tasks#new', as: :new_tasks
+#   post 'tasks', to: 'tasks#create'
+#   # READ ONE
+#   get "tasks/:id", to: "tasks#show", as: :task
+#   # UPDATE
+#   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+#   patch "tasks/:id", to: "tasks#update"
+#   # DELETE
+#   delete "tasks/:id", to: "tasks#destroy"
