b2cbf88e15e13700a1d7e0f1633b58ee879cd273
Marcus Agbaglah <magbaglah360@gmail.com>
2022-09-01 11:22:40 +0100

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -5,26 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  # get "vans", to: "vans#index", as: :vans
-  # get "vans/new", to: "vans#new", as: :new_vans
-  # get "vans/:id", to: "vans#show", as: :van
-  # post "vans", to: "vans#create"
-  resources :vans, only: [:index, :show, :new, :create]
+  resources :vans, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
 end
-
-
-
-# # CRUD
-#   # READ ALL
-#   get 'tasks', to: 'tasks#index', as: :index
-#   # CREATE
-#   get 'tasks/new', to: 'tasks#new', as: :new_tasks
-#   post 'tasks', to: 'tasks#create'
-#   # READ ONE
-#   get "tasks/:id", to: "tasks#show", as: :task
-#   # UPDATE
-#   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-#   patch "tasks/:id", to: "tasks#update"
-#   # DELETE
-#   delete "tasks/:id", to: "tasks#destroy"
